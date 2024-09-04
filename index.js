70b1789fccb2999b7f4b4a713a008b1a8bbc9c75
Arnau Ros <arnauros22@gmail.com>
2024-09-04 15:39:47 +0200

    Your commit message

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const mapboxToken =
   "pk.eyJ1IjoiYXJuYXVyb3MiLCJhIjoiY20wYXNqOTU2MDEzYzJtc2Q0MXRpMjlnciJ9.UPU3udIJIprlj7HXDDgrbQ";
 let map;
 
+console.log("Script loaded");
+
 document.addEventListener("DOMContentLoaded", function () {
   console.log("DOMContentLoaded event fired");
   initializeMap();
